dc618281a27df855a4ecea85f7016ebf034ceaa2
jonybang <Jonybange@gmail.com>
2018-08-17 21:55:00 +0200

    deploy

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -3,7 +3,10 @@ const SpaceToken = artifacts.require('./SpaceToken');
 const LandUtils = artifacts.require('./LandUtils');
 const PlotManager = artifacts.require('./PlotManager');
 const SplitMerge = artifacts.require('./SplitMerge');
-const AdminUpgradeabilityProxy = artifacts.require('zos-lib/contracts/upgradeability/AdminUpgradeabilityProxy.sol');
+const Web3 = require('web3');
+// const AdminUpgradeabilityProxy = artifacts.require('zos-lib/contracts/upgradeability/AdminUpgradeabilityProxy.sol');
+
+const fs = require('fs');
 
 module.exports = async function(deployer, network, accounts) {
   if (network === 'test' || network === 'local' || network === 'development') {
@@ -11,40 +14,87 @@ module.exports = async function(deployer, network, accounts) {
     return;
   }
 
-  const coreTeam = accounts[0];
-  const proxiesAdmin = accounts[1];
+  deployer.then(async () => {
+    const coreTeam = accounts[0];
+    // const proxiesAdmin = accounts[1];
 
-  // Deploy contracts...
-  await deployer.deploy(GaltToken, { from: coreTeam });
-  await deployer.deploy(SpaceToken, 'Space Token', 'SPACE', { from: coreTeam });
-  await deployer.deploy(SplitMerge, { from: coreTeam });
-  await deployer.deploy(PlotManager, { from: coreTeam });
-  await deployer.deploy(LandUtils, { from: coreTeam });
+    // Deploy contracts...
+    console.log('Deploy contracts...');
+    const galtToken = await GaltToken.new({ from: coreTeam });
+    const spaceToken = await SpaceToken.new('Space Token', 'SPACE', { from: coreTeam });
+    const splitMerge = await SplitMerge.new({ from: coreTeam });
+    const plotManager = await PlotManager.new({ from: coreTeam });
+    const landUtils = await LandUtils.new({ from: coreTeam });
 
-  // Setup proxies...
-  // NOTICE: The address of a proxy creator couldn't be used in the future for logic contract calls.
-  // https://github.com/zeppelinos/zos-lib/issues/226
-  const spaceTokenProxy = await AdminUpgradeabilityProxy.new(SpaceToken.address, { from: proxiesAdmin });
-  const splitMergeProxy = await AdminUpgradeabilityProxy.new(SplitMerge.address, { from: proxiesAdmin });
-  const plotManagerProxy = await AdminUpgradeabilityProxy.new(PlotManager.address, { from: proxiesAdmin });
-  const landUtilsProxy = await AdminUpgradeabilityProxy.new(LandUtils.address, { from: proxiesAdmin });
+    // Setup proxies...
+    // NOTICE: The address of a proxy creator couldn't be used in the future for logic contract calls.
+    // https://github.com/zeppelinos/zos-lib/issues/226
+    // const spaceTokenProxy = await AdminUpgradeabilityProxy.new(SpaceToken.address, { from: proxiesAdmin });
+    // const splitMergeProxy = await AdminUpgradeabilityProxy.new(SplitMerge.address, { from: proxiesAdmin });
+    // const plotManagerProxy = await AdminUpgradeabilityProxy.new(PlotManager.address, { from: proxiesAdmin });
+    // const landUtilsProxy = await AdminUpgradeabilityProxy.new(LandUtils.address, { from: proxiesAdmin });
+    //
+    // // Instantiate logic contract at proxy addresses...
+    // await SpaceToken.at(spaceTokenProxy.address);
+    // await SplitMerge.at(splitMergeProxy.address);
+    // await PlotManager.at(plotManagerProxy.address);
+    // await LandUtils.at(landUtilsProxy.address);
 
-  // Instantiate logic contract at proxy addresses...
-  const spaceToken = await SpaceToken.at(spaceTokenProxy.address);
-  const splitMerge = await SplitMerge.at(splitMergeProxy.address);
-  const plotManager = await PlotManager.at(plotManagerProxy.address);
-  const landUtils = await LandUtils.at(landUtilsProxy.address);
+    // Call initialize methods (constructor substitute for proxy-backed contract)
+    await spaceToken.initialize('Space Token', 'SPACE', { from: coreTeam });
+    await spaceToken.addRoleTo(plotManager.address, 'minter', { from: coreTeam });
+    await spaceToken.addRoleTo(splitMerge.address, 'minter');
+    await spaceToken.addRoleTo(splitMerge.address, 'operator');
 
-  // Call initialize methods (constructor substitute for proxy-backed contract)
-  spaceToken.initialize(plotManager.address, 'Space Token', 'SPACE', { from: coreTeam });
-  spaceToken.setSplitMerge(splitMerge.address, { from: coreTeam });
-  splitMerge.initialize(spaceToken.address, { from: coreTeam });
-  plotManager.initialize(spaceToken.address, splitMerge.address, { from: coreTeam });
-  landUtils.initialize({ from: coreTeam });
+    await splitMerge.initialize(spaceToken.address, plotManager.address, { from: coreTeam });
 
-  // Log out proxy addresses
-  console.log('SpaceToken Proxy:', spaceToken.address);
-  console.log('SplitMerge Proxy:', splitMerge.address);
-  console.log('PlotManager Proxy:', plotManager.address);
-  console.log('LandUtils Proxy:', landUtils.address);
+    await plotManager.initialize(
+      Web3.utils.toWei('0.1', 'ether'),
+      '25',
+      coreTeam,
+      spaceToken.address,
+      splitMerge.address,
+      {
+        from: coreTeam
+      }
+    );
+
+    await landUtils.initialize({ from: coreTeam });
+
+    await plotManager.addValidator(
+      '0xf0430bbb78c3c359c22d4913484081a563b86170',
+      Web3.utils.utf8ToHex('Jonybang'),
+      Web3.utils.utf8ToHex('RU'),
+      { from: coreTeam }
+    );
+
+    await new Promise(resolve => {
+      fs.writeFile(
+        `${__dirname}/../deployed_${network}.json`,
+        JSON.stringify(
+          {
+            galtTokenAddress: galtToken.address,
+            galtTokenAbi: galtToken.abi,
+            spaceTokenAddress: spaceToken.address,
+            spaceTokenAbi: spaceToken.abi,
+            splitMergeAddress: splitMerge.address,
+            splitMergeAbi: splitMerge.abi,
+            plotManagerAddress: plotManager.address,
+            plotManagerAbi: plotManager.abi,
+            landUtilsAddress: landUtils.address,
+            landUtilsAbi: landUtils.abi
+          },
+          null,
+          2
+        ),
+        resolve
+      );
+    });
+
+    // Log out proxy addresses
+    console.log('SpaceToken Proxy:', spaceToken.address);
+    console.log('SplitMerge Proxy:', splitMerge.address);
+    console.log('PlotManager Proxy:', plotManager.address);
+    console.log('LandUtils Proxy:', landUtils.address);
+  });
 };
