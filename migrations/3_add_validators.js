da89c00199d7580486f4e634ae7d51d1901947c1
jonybang <Jonybange@gmail.com>
2018-08-23 19:32:35 +0200

    change source data

--- migrations/3_add_validators.js
+++ migrations/3_add_validators.js
@@ -25,7 +25,8 @@ module.exports = async function(deployer, network, accounts) {
       Jonybang: '0xf0430bbb78c3c359c22d4913484081a563b86170',
       Nikita: '0x8d362af4c86b05d6F256147A6E76b9d7aF205A24',
       Igor: '0x06dba6eb6a1044b8cbcaa0033ea3897bf37e6671',
-      Nik: '0x83d61498cc955c4201042f12bd34e818f781b90b'
+      Nik: '0x486129f16423bb74786abc99eab06897f73310f5',
+      Nik2: '0x83d61498cc955c4201042f12bd34e818f781b90b'
     };
 
     const rewarder = accounts[3] || accounts[2] || accounts[1] || accounts[0];
